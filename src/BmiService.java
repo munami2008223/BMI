5bab218f2d9f9f9244437388462e7df7491c5f87
Natali <munami200823@yandex.ru>
2024-12-02 21:09:22 +0500

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,9 @@
+public class BmiService {
+    public int calculate(double humanHeight, double humanWeight)
+
+    {
+        double bodyMassIndex = humanWeight / (humanHeight * humanHeight);
+        return (int) bodyMassIndex;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService ();
+        double humanHeight = 1.87; // метра
+        double humanWeight = 98; // килограммов
+        double bodyMassIndex = service.calculate(humanHeight, humanWeight);
+        System.out.println(bodyMassIndex);
+    }
+}
